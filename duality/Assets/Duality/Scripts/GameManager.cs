2fb7fd65c411da18ab2e28d5ba96c77ac0830aab
michael <insominx@yahoo.com>
2022-01-22 13:27:28 -0800

    Added test for transitioning to vignette.  Added clouds.  Fixed some UI positioning.

--- duality/Assets/Duality/Scripts/GameManager.cs
+++ duality/Assets/Duality/Scripts/GameManager.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Player")]
+    public Transform player;
+    public Transform vignettePlayerStart;
 
-    // Update is called once per frame
+    [Header ("Camera")]
+    public CameraFollow cameraController;
+    public Transform vignetteCameraStart;
+    public float vignetteTransitionDuration = 1.5f;
+
+    //-----------------------------------------------------------------------------
     void Update()
     {
-        
+        // DEBUG
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartCoroutine(StartVignette());
+        }
+    }
+
+    //-----------------------------------------------------------------------------
+    IEnumerator StartVignette()
+    {
+        cameraController.enabled = false;
+        float timeElapsed = 0f;
+        Camera camera = cameraController.GetComponent<Camera>();
+        Vector3 startPos = camera.transform.position;
+
+        StartCoroutine(TeleportPlayerAfterDelay(vignetteTransitionDuration / 2f));
+
+        while (timeElapsed < vignetteTransitionDuration)
+        {
+            float alpha = timeElapsed / vignetteTransitionDuration;
+            camera.transform.position = Vector3.Lerp(startPos, vignetteCameraStart.position, alpha);
+            yield return null;
+
+            timeElapsed += Time.deltaTime;
+        }
+
+        camera.transform.position = vignetteCameraStart.position;
+    }
+
+    //-----------------------------------------------------------------------------
+    IEnumerator TeleportPlayerAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        player.position = vignettePlayerStart.position;
     }
 }
