83977997b364549017ffcb83b9e8f2b0b5d5379b
michael <insominx@yahoo.com>
2022-01-22 17:11:07 -0800

    Move Camera stuff to CameraManager.  Made page layout better tolerate different resolutions.

---
+++ duality/Assets/Duality/Scripts/CameraManager.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.U2D;
+
+public class CameraManager : MonoBehaviour
+{
+    [Header("Player")]
+    public Transform player;
+    public Transform vignettePlayerStart;
+
+    [FormerlySerializedAs("cameraController")]
+    [Header("Camera")]
+    public CameraFollow cameraFollow;
+    public PixelPerfectCamera pixelCamera;
+    public float vignetteTransitionDuration = 1.5f;
+
+    [Header("UI")]
+    public Transform angelDemonRoot;
+
+    //-----------------------------------------------------------------------------
+    private void Start()
+    {
+        pixelCamera.refResolutionX = Screen.width - 420;
+        pixelCamera.refResolutionY = Screen.height - 420;
+    }
+
+    //-----------------------------------------------------------------------------
+    void Update()
+    {
+        // DEBUG
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartVignette();
+        }
+    }
+
+    //-----------------------------------------------------------------------------
+    void StartVignette()
+    {
+        _WarpPlayerToNewLocation();
+        _LerpCameraToNewLocation();
+        _LerpUIOffscreen();
+
+        void _WarpPlayerToNewLocation()
+        {
+            // Warp the player to vignette location once out of camera view
+            var sequence = DOTween.Sequence();
+            sequence.AppendInterval(vignetteTransitionDuration * 0.6f);
+            sequence.Append(player.DOMove(vignettePlayerStart.position, 0f));
+        }
+
+        void _LerpCameraToNewLocation()
+        {
+            Camera camera = cameraFollow.GetComponent<Camera>();
+            cameraFollow.enabled = false;
+
+            // We want to go to where the player is but maintain our z value
+            var newCameraPosition = vignettePlayerStart.position;
+            newCameraPosition.z = camera.transform.position.z;
+
+            // Lerp the camera through the clouds to the vignette area
+            var cameraTween = camera.transform.DOMove(newCameraPosition, vignetteTransitionDuration);
+            // cameraTween.SetEase(Ease.InCubic); //<-- change interpolation type
+
+            // restore camera control when done
+            cameraTween.OnComplete((() =>
+            {
+                cameraFollow.enabled = true;
+            }));
+        }
+
+        void _LerpUIOffscreen()
+        {
+            Vector3 uiPosition = angelDemonRoot.position;
+            angelDemonRoot.DOMove(uiPosition + Vector3.up * 20f, vignetteTransitionDuration);
+        }
+    }
+}
